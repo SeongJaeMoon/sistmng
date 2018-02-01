ca443fe441ad26c1aee904314ff95f83e9ea8199
youmekko <youmechoi1129@gmail.com>
2018-02-01 16:34:42 +0900

    first commit

--- src/com/sistmng/instructor/InstructorDAO.java
+++ src/com/sistmng/instructor/InstructorDAO.java
@@ -1,5 +1,121 @@
 package com.sistmng.instructor;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class InstructorDAO {
+
+	public String instructorInfo() {
+		StringBuilder sb = new StringBuilder();
+
+		return sb.toString();
+	}
+
+	public List<Instructor> courseList() {
+
+		List<Instructor> courseList = new ArrayList<Instructor>();
+
+		return courseList;
+
+	}
+
+	public Instructor selectedSubject(String subCode) {
+		Instructor i = new Instructor();
+
+		String sql = "";
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			// conn = DBConnection.connect();
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, subCode);
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+
+				i.setOpenSubCode(rs.getString("openSubCode"));
+				i.setSubjectName(rs.getString("subjectName"));
+				i.setOpenCourseStartDate(rs.getDate("openSubStarDate").toLocalDate());
+				i.setOpenCourseCloseDate(rs.getDate("openCourseStartDate").toLocalDate());
+
+			}
+			rs.close();
+
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (SQLException se) {
+			}
+			try {
+				// DBConnection.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+		
+
+		return i;
+	}
+
+	public List<Instructor> studentListByCourse(String subCode) {
+
+		List<Instructor> studentListByCourse = new ArrayList<Instructor>();
+
+		String sql = "";
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			// conn = DBConnection.connect();
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, subCode);
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+
+				Instructor i = new Instructor();
+
+				i.setMid(rs.getString("mid"));
+				i.setName_(rs.getString("name_"));
+				i.setPhone(rs.getString("phone"));
+				// i.setStudentStatus();
+
+			}
+			rs.close();
+
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (SQLException se) {
+			}
+			try {
+				// DBConnection.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+
+		return studentListByCourse;
+	}
 
 }
--- src/com/sistmng/instructor/InstructorService.java
+++ src/com/sistmng/instructor/InstructorService.java
@@ -1,5 +1,55 @@
 package com.sistmng.instructor;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class InstructorService {
+
+	private InstructorDAO insDAO = new InstructorDAO();
+
+	public void menu_1(Scanner sc) {
+
+		System.out.println(this.insDAO.instructorInfo());
+	}
+
+	public void menu_2(Scanner sc) {
+
+		List<Instructor> courseList = this.insDAO.courseList();
+
+		System.out.println(
+				"-----------------------------------------------------------------------------------------------------------------------------------");
+
+		System.out.println("과목코드 / 과목명 / 과목 시작일 / 과목 종료일 / 교재명/ 과정코드 / 과정명 / 과정 시작일/ 과정 종료일 / 강의실 / 등록인원 / 상태");
+		System.out.println(
+				"-----------------------------------------------------------------------------------------------------------------------------------");
+
+		for (Instructor i : courseList) {
+			System.out.printf("%s / %s / %s / %s / %s / %s / %s / %s / %s / %s / %s / %s%n", i.getOpenSubCode(),
+					i.getSubjectName(), i.getOpenCourseStartDate(), i.getOpenCourseCloseDate(), i.getBookName(),
+					i.getOpenCocode(), i.getCourserName(), i.getOpenCourseStartDate(), i.getOpenCourseCloseDate(),
+					i.getClassName(), i.getOpenCoStatus());
+
+		}
+
+		System.out.print("과목코드 >");
+
+		String subCode = sc.nextLine();
+
+		Instructor selectedSubject = this.insDAO.selectedSubject(subCode);
+		System.out.printf("[ %s / %s / %s ~ %s ]%n", selectedSubject.getOpenSubCode(), selectedSubject.getSubjectName(),
+				selectedSubject.getOpenSubStartDate(), selectedSubject.getOpenSubcloseDate());
+
+		List<Instructor> studentListbyCourse = this.insDAO.studentListByCourse(subCode);
+
+		System.out.println("--------------------------------------------");
+		System.out.println("회원코드 / 이름 / 전화번호 / 등록일 / 수료");
+		System.out.println("--------------------------------------------");
+
+		for (Instructor i : studentListbyCourse) {
+			System.out.printf("%s / %s / %s / %s / %s%n", i.getMid(), i.getName_(), i.getPhone(),
+					i.getInstructorRegDate(), i.getStudentStatus());
+		}
+
+	}
 
 }
